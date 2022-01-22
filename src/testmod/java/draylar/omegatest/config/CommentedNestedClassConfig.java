b46abd798d63eb3799df65fe88f517f73024ca40
alkyaly <73117859+alkyaly@users.noreply.github.com>
2022-01-22 19:27:35 -0300

    Greatly simplify commented nested config test

--- src/testmod/java/draylar/omegatest/config/CommentedNestedClassConfig.java
+++ src/testmod/java/draylar/omegatest/config/CommentedNestedClassConfig.java
@@ -2,6 +2,7 @@ package draylar.omegatest.config;
 
 import draylar.omegaconfig.api.Comment;
 import draylar.omegaconfig.api.Config;
+import org.jetbrains.annotations.Nullable;
 
 public class CommentedNestedClassConfig implements Config {
 
@@ -11,9 +12,6 @@ public class CommentedNestedClassConfig implements Config {
     @Comment("True")
     public boolean test = false;
 
-    @Comment("First-Level")
-    public VeryNested veryNested = new VeryNested();
-
     public static class First {
         @Comment("Cucumbers")
         public boolean cucumbers = true;
@@ -21,49 +19,20 @@ public class CommentedNestedClassConfig implements Config {
         @Comment("InnerFirst")
         public InnerFirst innerFirst = new InnerFirst();
 
-        @Comment("InnerSecond")
-        public InnerSecond innerSecond = new InnerSecond();
-
         public static class InnerFirst {
-            @Comment("Number of Tomatoes")
-            public int numberOfTomatoes = 10;
-        }
-
-        public static class InnerSecond {
-            @Comment("Inner Second First")
-            public InnerSecondFirst innerSecondFirst = new InnerSecondFirst();
-
-            public static class InnerSecondFirst {
-                @Comment("Olive Oil")
-                public String oliveOil = "oliveOil";
-            }
-        }
-    }
-
-    public static class VeryNested {
-        @Comment("Second-Level")
-        public Very very = new Very();
-        public static class Very {
-            @Comment("Third-Level")
-            public VeryVery veryVery = new VeryVery();
-            public static class VeryVery {
-                @Comment("Forth-Level")
-                public VeryVeryVery veryVeryVery = new VeryVeryVery();
-                public static class VeryVeryVery {
-                    @Comment("Fifth-Level")
-                    public VeryVeryVeryVery veryVeryVeryVery = new VeryVeryVeryVery();
-                    public static class VeryVeryVeryVery {
-                        @Comment("Sixth-Level")
-                        public String last = "last";
-                    }
-                }
-            }
+            @Comment("e")
+            public boolean e = false;
         }
     }
 
     @Override
     public String getName() {
         return "commented-inner-classes";
+    }
+
+    @Override
+    public @Nullable String getModid() {
+        return "omega-config-gui";
     }
 
     @Override
