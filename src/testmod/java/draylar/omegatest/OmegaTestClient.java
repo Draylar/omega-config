5f19348fdc4833cebee4e9591952742da1a1f8c3
Draylar <17728338+Draylar@users.noreply.github.com>
2023-07-01 16:30:59 -0500

    1.20.1 update without Omega Config GUI

--- src/testmod/java/draylar/omegatest/OmegaTestClient.java
+++ src/testmod/java/draylar/omegatest/OmegaTestClient.java
@@ -1,20 +1,19 @@
 package draylar.omegatest;
 
-import draylar.omegaconfiggui.OmegaConfigGui;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.text.Text;
 
 public class OmegaTestClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        OmegaConfigGui.registerConfigScreen(OmegaTestMain.CONFIG);
-
-        HudRenderCallback.EVENT.register((stack, delta) -> {
-            MinecraftClient.getInstance().textRenderer.draw(stack, Text.literal(String.valueOf(OmegaTestMain.CONFIG.v)), 15, 15, 0xffffff);
-            MinecraftClient.getInstance().textRenderer.draw(stack, Text.literal(String.valueOf(OmegaTestMain.CONFIG.doubleTest)), 15, 30, 0xffffff);
+        HudRenderCallback.EVENT.register((context, delta) -> {
+            TextRenderer tr = MinecraftClient.getInstance().textRenderer;
+            context.drawText(tr, Text.literal(String.valueOf(OmegaTestMain.CONFIG.v)), 15, 15, 0xffffff, true);
+            context.drawText(tr, Text.literal(String.valueOf(OmegaTestMain.CONFIG.doubleTest)), 15, 25, 0xffffff, true);
         });
     }
 }
